9e17df6f7e9c8aaf0a4fb561884011ada41196d4
Tim <timothy.james.dymond@gmail.com>
2025-05-14 11:06:07 +1000

    changed list to a table, added date, priority,

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 function addListItem() {
     // This function adds a new item to the list
-    item = document.getElementById("todoInput");
     list = document.getElementById("todoList");
-    listItem = document.createElement("li");
-    listItem.innerHTML = item.value;
-    list.appendChild(listItem);
+    
+    date = document.getElementById("dateInput").value;
+    item = document.getElementById("todoInput");
+    priority = document.getElementById("priorityInput").value;
+    
+    //create table items
+    row = list.insertRow();
+    dateCell = row.insertCell(0);
+    taskCell = row.insertCell(1);
+    PriorityCell = row.insertCell(2);
+    deleteCell = row.insertCell(3);
+
+    //put data into table items
+    dateCell.innerHTML = date;
+    taskCell.innerHTML = item.value;
+    priorityCell.innerHTML = priority;
+
     item.value = "";
+    date.value = "";
+    priority.value = "";
+
 }
 
 document.getElementById("todoInput").addEventListener("keypress", function(event) {
+    // Check if the Enter key is pressed
+    if (event.key === "Enter") {
+        addListItem();
+    }
+});
+
+document.getElementById("dateInput").addEventListener("keypress", function(event) {
     // Check if the Enter key is pressed
     if (event.key === "Enter") {
         addListItem();
